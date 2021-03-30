744610cfed7ed5be23b463ed3599bf30ac5234c5
TatianaMG <tatiana.morales.gonzalez@gmail.com>
2021-03-30 16:22:58 +0200

    Bootstrap popover function has been tested

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,6 @@ import './bootstrap';
 const $ = require('jquery');
 require('bootstrap');
 
-$(document).ready(function() { /*deprecation*/
+$(function() {
     $('[data-toggle="popover"]').popover();
 });
